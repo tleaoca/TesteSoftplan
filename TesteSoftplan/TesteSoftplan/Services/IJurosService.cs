2bcefd87d879577d5a54296fc104cd218c3ee484
Thiago Leao <tleaoca@gmail.com>
2021-08-12 21:56:34 -0300

    atualizacoes

---
+++ TesteSoftplan/TesteSoftplan/Services/IJurosService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TesteSoftplan.Data;
+
+namespace TesteSoftplan.Services
+{
+    public interface IJurosService
+    {
+        string calculoJ([FromBody] Juros juros);
+        decimal mostrarJ();
+        string retornarUrl(string urlGithub);
+    }
+}
